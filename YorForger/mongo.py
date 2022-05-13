4f0166313899fc40280846e294265c21c826fefd
Yash <yashprakash2005@gmail.com>
2022-05-13 16:37:25 +0530

    make it better

--- YorForger/mongo.py
+++ YorForger/mongo.py
@@ -1,21 +1,20 @@
 import asyncio
 import sys
-
 from motor import motor_asyncio
-from YorForger import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from YorForger.conf import get_int_key, get_str_key
+from YorForger import MONGO_DB_URI
+from YorForger.confing import get_int_key, get_str_key
+from YorForger.utlis.logger import log
 
-
-
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
+MONGO_PORT = get_int_key("27017")
+MONGO_DB_URI = get_str_key("MONGO_URI")
 MONGO_DB = "Yor"
 
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
+client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
 db = client["Yor"]
 try:
