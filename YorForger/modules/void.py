d0be085e5212eccffc4f95e5c211f0565eaab41c
Yash <yashprakash2005@gmail.com>
2022-05-30 01:17:57 +0530

    Update void.py

--- YorForger/modules/void.py
+++ YorForger/modules/void.py
@@ -5,14 +5,14 @@ PHOTO = "https://telegra.ph/file/f9b0895ae78578fda9202.jpg"
 
 @pbot.on_message(filters.command(['void']))
 async def void(_, message):
-    caption = f"**Welcome to [【V๏ɪ፝֟𝔡】Network](https://t.me/voidxnetwork)** \n\n◈ Void is an anime based Community with a motive to spread love and peace around telegram. Go through the channel and join the Community if it draws your attention. ◈"
+    caption = f"Welcome to [【V๏ɪ፝֟𝔡】Network](https://t.me/voidxnetwork) \n\n◈ Void is an anime based Community with a motive to spread love and peace around telegram. Go through the channel and join the Community if it draws your attention. ◈"
 
     await message.reply_photo(photo=PHOTO, caption=caption)
 
 
-__help__ = """
+help = """
  ──「Void Network」──                         
  
 ❂ /void: Get information about our community! using it in groups may create promotion so we don't support using it in groups."""
    
-__mod_name__ = "【ᴠᴏɪᴅ】"
+mod_name = "【ᴠᴏɪᴅ】"
