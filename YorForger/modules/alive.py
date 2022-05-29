4b8e5e35bface6bfa104d01869bf6d3154cbf6cd
Yash <yashprakash2005@gmail.com>
2022-05-30 05:15:12 +0530

    hmm

--- YorForger/modules/alive.py
+++ YorForger/modules/alive.py
@@ -9,7 +9,7 @@ from telegram.ext import (
 PHOTO = "https://telegra.ph/file/fa2c031ecf5df678280e5.jpg"
 
 def alive(update: Update, context: CallbackContext):
-    TEXT = f"**I Am Kita Shinsuke!**\n\n**I Work Under - [Void X Network](t.me/voidxnetwork)** \n\n◈ I will love to be in your group chat ◈"
+    TEXT = f"I Am Kita Shinsuke!\n\nI Work Under - 【V๏ɪ፝֟𝔡】 ✧Network✧ \n\n◈ I will love to be in your group chat ◈"
     
 
     update.effective_message.reply_photo(
