aa589d69f2a0b2681ce10ee3257ec944852ac099
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-05-29 17:18:38 +0530

    Update userinfo.py

--- YorForger/modules/userinfo.py
+++ YorForger/modules/userinfo.py
@@ -26,7 +26,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.error import BadRequest
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from horisan import (
+from YorForger import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
