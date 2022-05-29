6c1bb7b54e2fa513161c0862280327ff15565d0c
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-05-29 14:44:21 +0530

    Update alive.py

--- YorForger/modules/alive.py
+++ YorForger/modules/alive.py
@@ -5,23 +5,23 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from horisan.events import register
-from horisan import telethn as tbot
+from YorForger.events import register
+from YorForger import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/c458924bf40c213062dd2.mp4"
+PHOTO = "https://telegra.ph/file/dbf5b416000d948b0fdca.jpg"
 
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**Hey There Weebs!![{event.sender.first_name}](tg://user?id={event.sender.id})** \n\n"
-    TEXT += f"**I Am Hori San!**\n\n"
-    TEXT += f"**Meet My izumi kun hehe - [VOID](https://t.me/voidxtoxic)**** \n\n"
-    TEXT += "**◈ I will love to be in your groupchat ◈"
+    TEXT = f"**Hey There Team Mates!![{event.sender.first_name}](tg://user?id={event.sender.id})** \n\n"
+    TEXT += f"**I Am Kita Shinsuke!**\n\n"
+    TEXT += f"**I Work Under - [Void X Network](t.me/voidxnetwork)**** \n\n"
+    TEXT += "◈ I will love to be in your groupchat ◈"
     BUTTON = [
         [
-            Button.url("【Support】", "https://t.me/HoriXSupport"),
-            Button.url("【Updates】", "https://t.me/kyouko_update"),
+            Button.url("【Support】", "https://t.me/kitaxsupport"),
+            Button.url("【Updates】", "https://t.me/kitaxupdates"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
