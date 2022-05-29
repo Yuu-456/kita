672f981e56a825e2a1acb7b6ceae0bfe747f030a
Yash <yashprakash2005@gmail.com>
2022-05-30 00:31:00 +0530

    Update void.py

--- YorForger/modules/void.py
+++ YorForger/modules/void.py
@@ -5,7 +5,7 @@ from pyrogram.types import (ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKey
 PHOTO = "https://telegra.ph/file/f9b0895ae78578fda9202.jpg"
 
 @pbot.on_message(filters.command(['void']))
-async def void(_, message):
+async def void(message):
     caption = f"**Welcome to [【V๏ɪ፝֟𝔡】Network](https://t.me/voidxnetwork)** \n\n◈ Void is an anime based Community with a motive to spread love and peace around telegram. Go through the channel and join the Community if it draws your attention. ◈"
     buttons = [
         [
