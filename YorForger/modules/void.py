9fec4b9a26eb3b6b662007e291073fad744797d0
Yash <yashprakash2005@gmail.com>
2022-05-29 21:57:08 +0530

    Update void.py

--- YorForger/modules/void.py
+++ YorForger/modules/void.py
@@ -1,6 +1,8 @@
 from pyrogram import filters
 from YorForger import pbot
 
+PHOTO = "https://telegra.ph/file/f9b0895ae78578fda9202.jpg"
+
 @pbot.on_message(filters.command(['void']))
 async def void(_, message):
     caption = f"**Welcome to [【V๏ɪ፝֟𝔡】Network](https://t.me/voidxnetwork)** \n\n◈ Void is an anime based Community with a motive to spread love and peace around telegram. Go through the channel and join the Community if it draws your attention. ◈"
