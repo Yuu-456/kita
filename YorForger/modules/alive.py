4b713776e7fa302b92d1c84129fca214ef772a1f
Yash <yashprakash2005@gmail.com>
2022-05-30 01:10:49 +0530

    hmm

--- YorForger/modules/alive.py
+++ YorForger/modules/alive.py
@@ -1,56 +1,28 @@
-# import os
-# import re
-# from platform import python_version as kontol
-# from telethon import events, Button
-# from telegram import __version__ as telever
-# from telethon import __version__ as tlhver
-# from pyrogram import __version__ as pyrover
-# from YorForger.events import register
-# from YorForger import telethn as tbot
-
-
-# PHOTO = "https://telegra.ph/file/fa2c031ecf5df678280e5.jpg"
-
-
-# @register(pattern=("/alive"))
-# async def awake(event):
-#     TEXT = f"**Hey There Team Mates!![{event.sender.first_name}](tg://user?id={event.sender.id})** \n\n"
-#     TEXT += f"**I Am Kita Shinsuke!**\n\n"
-#     TEXT += f"**I Work Under - [Void X Network](t.me/voidxnetwork)**** \n\n"
-#     TEXT += "◈ I will love to be in your groupchat ◈"
-#     BUTTON = [
-#         [
-#             Button.url("【Support】", "https://t.me/kitaxsupport"),
-#             Button.url("【Updates】", "https://t.me/kitaxupdates"),
-#         ]
-#     ]
-#     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
-
-
 from pyrogram import filters
 from YorForger import pbot
-from telethon import Button
 
 PHOTO = "https://telegra.ph/file/f9b0895ae78578fda9202.jpg"
 
 @pbot.on_message(filters.command(['void']))
-async def awake(event):
+async def void(_, message):
     caption = f"**I Am Kita Shinsuke!**\n\n**I Work Under - [Void X Network](t.me/voidxnetwork)** \n\n◈ I will love to be in your groupchat ◈"
+    
 
-    # BUTTON = [
-    #     [
-    #         Button.url("【Support】", "https://t.me/kitaxsupport"),
-    #         Button.url("【Updates】", "https://t.me/kitaxupdates"),
-    #     ]
-    # ]
-    await pbot.reply_photo(event.chat_id, PHOTO, caption=caption,
-    # buttons=BUTTON
-    )
+    await message.reply_photo(photo=PHOTO, caption=caption)
 
 
-help = """
- ──「Void Network」──                         
- 
-❂ /void: Get information about our community! using it in groups may create promotion so we don't support using it in groups."""
+    # # BUTTON = [
+    # #     [
+    # #         Button.url("【Support】", "https://t.me/kitaxsupport"),
+    # #         Button.url("【Updates】", "https://t.me/kitaxupdates"),
+    # #     ]
+    # # ]
+    # await pbot.reply_photo(event.chat_id, PHOTO, caption=caption,
+    # # buttons=BUTTON
+    # )
+
+
+help = """ 
+❂ /alive: To check if bot is alive or not."""
    
-mod_name = "【ᴠᴏɪᴅ】"
+mod_name = "Alive"
--- YorForger/modules/void.py
+++ YorForger/modules/void.py
@@ -1,25 +1,14 @@
 from pyrogram import filters
 from YorForger import pbot
-from pyrogram.types import (ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton)
-from telegram import ParseMode
 
 PHOTO = "https://telegra.ph/file/f9b0895ae78578fda9202.jpg"
 
 @pbot.on_message(filters.command(['void']))
-async def awake(message):
+async def void(_, message):
     caption = f"**Welcome to [【V๏ɪ፝֟𝔡】Network](https://t.me/voidxnetwork)** \n\n◈ Void is an anime based Community with a motive to spread love and peace around telegram. Go through the channel and join the Community if it draws your attention. ◈"
-    # buttons = [
-    #     [
-    #         InlineKeyboardButton(
-    #             " 【Usertag】", url="https://t.me/VoidxNetwork/3"),
-    #         InlineKeyboardButton(
-    #             "【Owner Sama】", url="https://t.me/voidaryan")
-    #         ]
-    # ]   
+    
 
-    await message.reply_photo(photo=PHOTO, caption=caption,
-    # reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.MARKDOWN,
-    )
+    await message.reply_photo(photo=PHOTO, caption=caption)
 
 
 __help__ = """
