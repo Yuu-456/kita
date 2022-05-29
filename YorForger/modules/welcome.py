79782e21bca790ac0e9998e40ea9726fb737be19
Yash <yashprakash2005@gmail.com>
2022-05-30 02:43:34 +0530

    Update welcome.py

--- YorForger/modules/welcome.py
+++ YorForger/modules/welcome.py
@@ -203,8 +203,9 @@ def new_member(update: Update, context: CallbackContext):  # sourcery no-metrics
 
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
+                text = f"Every One Behave Our **President** Is Here"
                 update.effective_message.reply_photo("https://telegra.ph/file/bc082487f0b8a3cace602.jpg",
-                    "Every One Behave Our <br>President</br> Is Here",
+                    text,
                     reply_to_message_id=reply,
                 )
                 welcome_log = (
@@ -214,11 +215,10 @@ def new_member(update: Update, context: CallbackContext):  # sourcery no-metrics
                 )
                 continue
 
-            # Welcome Devs
-            if new_mem.id in DEV_USERS:
-                if new_mem.id == 949365920:
+            if new_mem.id == 949365920:
+                    text = f"Every One Behave My **Head Developer**Is Here"
                     update.effective_message.reply_photo("https://telegra.ph/file/ff7ef0199a298b5518fc5.jpg",
-                        "Every One Behave My <br>Head Developer</br>Is Here",
+                        text,
                         reply_to_message_id=reply,
                     )
                     welcome_log = (
@@ -227,21 +227,26 @@ def new_member(update: Update, context: CallbackContext):  # sourcery no-metrics
                         f"Head Dev 'SOME1HING' just joined the chat"
                     )
                     continue
-                else:
-                    update.effective_message.reply_text(
-                    "Be Cool! My <br>Vice President</br> just joined the group!",
-                    reply_to_message_id=reply,
-                    )
-                    continue
-            if new_mem.id == DRAGONS:
+
+            # Welcome Devs
+            if new_mem.id in DEV_USERS:
+                text = f"Be Cool! My **Vice President** just joined the group!",
                 update.effective_message.reply_text(
-                    "My <br>ADVISOR</br> just joined the group!",
+                text,
+                reply_to_message_id=reply,
+                )
+                continue
+            if new_mem.id == DRAGONS:
+                text = f"My **ADVISOR** just joined the group!",
+                update.effective_message.reply_text(
+                    text,
                     reply_to_message_id=reply,
                     )
                 continue
             if new_mem.id == DEMONS:
+                text = f"My **Secretary** just joined the group!",
                 update.effective_message.reply_text(
-                    "My <br>Secretary</br> just joined the group!",
+                    text,
                     reply_to_message_id=reply,
                     )
                 continue
