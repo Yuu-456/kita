313dc3e254f3d3fc9549252174ca416bd4ceeb72
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-05-29 15:37:20 +0530

    Update void.py

--- YorForger/modules/void.py
+++ YorForger/modules/void.py
@@ -5,21 +5,21 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from horisan.events import register
-from horisan import telethn as tbot
+from YorForger.events import register
+from YorForger import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/7423dc037cb86e76c426d.jpg"
+PHOTO = "https://telegra.ph/file/f9b0895ae78578fda9202.jpg"
 
 
 @register(pattern=("/void"))
 async def awake(event):
-    TEXT = f"**Welcome to [【V๏ɪ፝֟𝔡】Network](https://t.me/void_network)** \n\n"
-    TEXT += "**◈ Void is an anime based Community with a motive to spread love and peace around telegram. Go through the channel and join the Community if it draws your attention. ◈"
+    TEXT = f"**Welcome to [【V๏ɪ፝֟𝔡】Network](https://t.me/voidxnetwork)** \n\n"
+    TEXT += "◈ Void is an anime based Community with a motive to spread love and peace around telegram. Go through the channel and join the Community if it draws your attention. ◈"
     BUTTON = [
         [
-            Button.url("【Usertag】", "https://t.me/void_network/103"),
-            Button.url("【Owner Sama】", "https://t.me/voidxtoxic"),
+            Button.url("【Usertag】", "https://t.me/VoidxNetwork/3"),
+            Button.url("【Owner Sama】", "https://t.me/voidaryan"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
