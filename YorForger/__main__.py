b3fdb68f69af4a70ec71255d51eec0306d73ebaf
Yash <yashprakash2005@gmail.com>
2022-05-30 17:08:40 +0530

    Update __main__.py

--- YorForger/__main__.py
+++ YorForger/__main__.py
@@ -419,6 +419,41 @@ def help_button(update, context):
     #             parse_mode=ParseMode.MARKDOWN,
     #             timeout=60,
     #     )
+def Shikimori_about_callback(update, context):
+    query = update.callback_query
+    if query.data == "Shikimori_":
+        query.message.edit_text(
+            text="๏ I'm *Shikimori*, a powerful group management bot built to help you manage your group easily."
+            "\n• I can restrict users."
+            "\n• I can greet users with customizable welcome messages and even set a group's rules."
+            "\n• I have an advanced anti-flood system."
+            "\n• I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc."
+            "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords."
+            "\n• I check for admins' permissions before executing any command and more stuffs"
+            "\n\n_Shikimori's licensed under the GNU General Public License v3.0_"
+            "\n\n Click on button bellow to get basic help for ShikimoriBot.",
+            parse_mode=ParseMode.MARKDOWN,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                 [
+                    InlineKeyboardButton(text="Back", callback_data="Kita_back")
+                 ]
+                ]   
+            ),
+        )
+    elif query.data == "Kita_back":
+        first_name = update.effective_user.first_name
+        uptime = get_readable_time((time.time() - StartTime))
+        query.message.edit_text(
+                PM_START_TEXT.format(
+                    escape_markdown(first_name)),
+                reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=ParseMode.MARKDOWN,
+                timeout=60,
+                disable_web_page_preview=False,
+        )
+
 
 
 @typing_action
